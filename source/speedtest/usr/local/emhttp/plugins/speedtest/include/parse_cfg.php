d583afbb66e50da3579f881bb3f1a903aad3898e
dmacias72 <derekmacias@live.com>
2018-01-15 19:59:18 -0700

    change from alert to warning

--- source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
+++ source/speedtest/usr/local/emhttp/plugins/speedtest/include/parse_cfg.php
@@ -2,7 +2,6 @@
 $s_cfg_file = '/boot/config/plugins/speedtest/speedtest.cfg';
 $s_cfg      = file_exists($s_cfg_file)  ? parse_ini_file($s_cfg_file)         : [];
 
-$s_alert    = isset($s_cfg['ALERT'])    ? htmlspecialchars($s_cfg['ALERT'])   : 'no';
 $s_download = isset($s_cfg['DOWNLOAD']) ? intval($s_cfg['DOWNLOAD'])          : 0;
 $s_list     = isset($s_cfg['LIST'])     ? htmlspecialchars($s_cfg['LIST'])    : 'auto';
 $s_notify   = isset($s_cfg['NOTIFY'])   ? htmlspecialchars($s_cfg['NOTIFY'])  : 'no';
@@ -14,7 +13,8 @@ $s_timeout  = isset($s_cfg['TIMEOUT'])  ? intval($s_cfg['TIMEOUT'])           : 
 $s_units    = isset($s_cfg['UNITS'])    ? htmlspecialchars($s_cfg['UNITS'])   : 'bits';
 $s_upload   = isset($s_cfg['UPLOAD'])   ? intval($s_cfg['UPLOAD'])            : 0;
 $s_version  = isset($s_cfg['VERSION'])  ? htmlspecialchars($s_cfg['VERSION']) : 'latest';
-$s_python   = (!file_exists('/usr/bin/python'));
+$s_warn     = isset($s_cfg['ALERT'])    ? htmlspecialchars($s_cfg['ALERT'])   : 'no';
+$s_python2  = (!file_exists('/usr/bin/python2'));
 $s_latest   = trim(shell_exec('/usr/bin/python /usr/local/emhttp/plugins/speedtest/scripts/speedtest-latest.py --version'));
 
 $s_filename = '/boot/config/plugins/speedtest/speedtest.xml';
